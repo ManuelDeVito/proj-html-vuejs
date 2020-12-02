c814a7a3dded8e0f1e94150ac997964840ce9b6c
Manuel <manuel.devito@icloud.com>
2020-12-02 16:39:23 +0100

    Header e Footer con Vue

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ var app = new Vue ({
 
     data: {
 
-        // logo: "logo-img-01.png"
+        logo_azienda_header: "img/logo-img-01.png",
+        menu: ['Home', 'Pages', 'Portfolio', 'Blog', 'Shop', 'Elements'],
+        logo_menu_header: "svg/svg-1.png",
+        logo_azienda_footer: "img/logo-img-01.png",
+        testo_footer: "Sometimes doing something seemingly demanding turns out to be fan. Here's the proof - Bryson, new brand your portfolio.",
+        icone_instagram: 'fa-instagram',
+        icone_facebook: 'fab fa-facebook-f',
+        icone_twitter: 'fab fa-twitter',
+        icone_pinterest: 'fab fa-pinterest-p',
+
 
     },
 
