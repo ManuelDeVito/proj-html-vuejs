4297fc5b6ee1ed5d35ec6224f021cdfc1f7d7787
Manuel <manuel.devito@icloud.com>
2020-12-02 17:03:08 +0100

    Aggiornamento Footer in Vue

--- main.js
+++ main.js
@@ -11,10 +11,8 @@ var app = new Vue ({
         logo_menu_header: "svg/svg-1.png",
         logo_azienda_footer: "img/logo-img-01.png",
         testo_footer: "Sometimes doing something seemingly demanding turns out to be fan. Here's the proof - Bryson, new brand your portfolio.",
-        icone_instagram: 'fa-instagram',
-        icone_facebook: 'fab fa-facebook-f',
-        icone_twitter: 'fab fa-twitter',
-        icone_pinterest: 'fab fa-pinterest-p',
+        icone_social: ['icon-social-instagram fa-instagram','icon-social-facebook fab fa-facebook-f','icon-social-twitter fab fa-twitter',
+        'icon-social-pinterest fab fa-pinterest-p']
 
 
     },
