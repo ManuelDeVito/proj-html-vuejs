3c5cc37b178f61c12c848b35fd595305dcc53405
Manuel <manuel.devito@icloud.com>
2020-12-08 19:56:22 +0100

    Modifiche struttura html x flex - Eliminato float - Miglioramenti css

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var app = new Vue ({
         indice_menu: 0,
         menu: ['Home', 'Pages', 'Portfolio', 'Blog', 'Shop', 'Elements'],
         logo_menu_header: "svg/svg-1.png",
-        logo_azienda_footer: "img/logo-img-01.png",
+        nome_azienda_footer: "Bryson",
         testo_footer: "Sometimes doing something seemingly demanding turns out to be fan. Here's the proof - Bryson, new brand your portfolio.",
         icone_social: ['icon-social-instagram fa-instagram','icon-social-facebook fab fa-facebook-f','icon-social-twitter fab fa-twitter',
         'icon-social-pinterest fab fa-pinterest-p']
